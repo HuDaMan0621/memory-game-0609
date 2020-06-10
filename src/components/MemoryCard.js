db4e9a4198a1ebad65a1218c4ad585c9e3953186
hudaman0621 <mrhu828@gmail.com>
2020-06-09 21:24:58 -0400

    onclick clicks

--- src/components/MemoryCard.js
+++ src/components/MemoryCard.js
@@ -4,11 +4,26 @@ import Logo from '../../src/dg.png';
 
 
 class MemoryCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { isFlipped: false };
+    }
+
+    clickHandler() {
+        this.setState({
+            isFlipped: !this.state.isFlipped 
+        })
+    };
 
     render() {
+        let innerClass = "MemoryCard_inner";
+        if (this.state.isFlipped) {
+            innerClass += ' flipped';
+        }
+
         return (
-            <div className="MemoryCard">
-                <div className="MemoryCard_inner">
+            <div className="MemoryCard" onClick={ (e) => this.clickHandler(e) }>
+                <div className={innerClass}>
                     <div className="MemoryCard_back"><img src={Logo} className="Logo" alt="bg" /></div>
                     <div className="MemoryCard_front">∆</div>
                 </div>
