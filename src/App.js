17864d45d6e2970ba66fafc62c6fea6a7027c0a8
hudaman0621 <mrhu828@gmail.com>
2020-06-10 13:14:29 -0400

    revert back to working state

--- src/App.js
+++ src/App.js
@@ -2,6 +2,19 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import MemoryCard from './components/MemoryCard';
+// import styles from './App.modules.css';
+// import styled from 'styled-components';
+// import { Container, Row, Col } from 'react-bootstrap';
+// import 'bootstrap/dist/css/bootstrap.css';
+
+// const Div = styled.div`
+//   color: red;
+//   backgoround-color: purple;
+// `
+// const H1 = styled.h1`
+//   font-size:100px;
+//   font-style: bold;
+// `
 
 function App() {
   return (
@@ -11,28 +24,28 @@ function App() {
         <h4 class="app-subtitle">Match Cards to win!</h4>
       </header>
       <div>
-        <MemoryCard />
-        <MemoryCard />
-        <MemoryCard />
-        <MemoryCard />
+        <MemoryCard></MemoryCard>
+        <MemoryCard></MemoryCard>
+        <MemoryCard></MemoryCard>
+        <MemoryCard></MemoryCard>
       </div>
       <div>
-        <MemoryCard />
-        <MemoryCard />
-        <MemoryCard />
-        <MemoryCard />
+        <MemoryCard></MemoryCard>
+        <MemoryCard></MemoryCard>
+        <MemoryCard></MemoryCard>
+        <MemoryCard></MemoryCard>
       </div>
       <div>
-        <MemoryCard />
-        <MemoryCard />
-        <MemoryCard />
-        <MemoryCard />
+        <MemoryCard></MemoryCard>
+        <MemoryCard></MemoryCard>
+        <MemoryCard></MemoryCard>
+        <MemoryCard></MemoryCard>
       </div>
       <div>
-        <MemoryCard />
-        <MemoryCard />
-        <MemoryCard />
-        <MemoryCard />
+        <MemoryCard></MemoryCard>
+        <MemoryCard></MemoryCard>
+        <MemoryCard></MemoryCard>
+        <MemoryCard></MemoryCard>
       </div>
     </div>
   );
