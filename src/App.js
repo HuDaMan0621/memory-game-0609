aedca10a0d8f10d31b1e738f5b88e8516d078a1b
hudaman0621 <mrhu828@gmail.com>
2020-06-10 15:50:43 -0400

    step1 finished

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import MemoryCard from './components/MemoryCard';
+
+function generateDeck() {
+  var symbol = ['∆', 'µ', 'å', '∫', 'ç', '∂', 'œ', '∑']; //1.2.a Set symbols equal to an array containing these characters: ∆, ß, £, §, •, $, +, ø
+  var deck = [];
+  for (let index = 0; index < 15; index++) {
+    //todo 
+    let element = deck[index];
+    let card = {
+      isFlipped: false,
+      symbol: symbol[index%8],
+    }
+  }
+  return shuffle(deck);
+};
+//1.8 copy and paste the shuffle method from stackover flow
+function shuffle(deck) {
+  for (let i = deck.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [deck[i], deck[j]] = [deck[j], deck[i]];
+  }
+  return deck;
+}
+
+
 // import styles from './App.modules.css';
 // import styled from 'styled-components';
 // import { Container, Row, Col } from 'react-bootstrap';
@@ -16,39 +40,53 @@ import MemoryCard from './components/MemoryCard';
 //   font-style: bold;
 // `
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Memory game</h1>
-        <h4 class="app-subtitle">Match Cards to win!</h4>
-      </header>
-      <div>
-        <MemoryCard></MemoryCard>
-        <MemoryCard></MemoryCard>
-        <MemoryCard></MemoryCard>
-        <MemoryCard></MemoryCard>
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      //step 1.2 Initialize the app state with 2 properties: deck and pickedCards
+      //and set them as empty array
+      deck: {}, //step 1.10 make the deck property point to a call to generateDeck()
+      pickedCards: {},
+    }
+
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Memory game</h1>
+          <h4 class="app-subtitle">Match Cards to win!</h4>
+        </header>
+        <div>
+          <MemoryCard></MemoryCard>
+          <MemoryCard></MemoryCard>
+          <MemoryCard></MemoryCard>
+          <MemoryCard></MemoryCard>
+        </div>
+        <div>
+          <MemoryCard></MemoryCard>
+          <MemoryCard></MemoryCard>
+          <MemoryCard></MemoryCard>
+          <MemoryCard></MemoryCard>
+        </div>
+        <div>
+          <MemoryCard></MemoryCard>
+          <MemoryCard></MemoryCard>
+          <MemoryCard></MemoryCard>
+          <MemoryCard></MemoryCard>
+        </div>
+        <div>
+          <MemoryCard></MemoryCard>
+          <MemoryCard></MemoryCard>
+          <MemoryCard></MemoryCard>
+          <MemoryCard></MemoryCard>
+        </div>
       </div>
-      <div>
-        <MemoryCard></MemoryCard>
-        <MemoryCard></MemoryCard>
-        <MemoryCard></MemoryCard>
-        <MemoryCard></MemoryCard>
-      </div>
-      <div>
-        <MemoryCard></MemoryCard>
-        <MemoryCard></MemoryCard>
-        <MemoryCard></MemoryCard>
-        <MemoryCard></MemoryCard>
-      </div>
-      <div>
-        <MemoryCard></MemoryCard>
-        <MemoryCard></MemoryCard>
-        <MemoryCard></MemoryCard>
-        <MemoryCard></MemoryCard>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
