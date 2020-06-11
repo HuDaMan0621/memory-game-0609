5c8f150afdde9f3f83aaeb232e01c58d16c17871
hudaman0621 <mrhu828@gmail.com>
2020-06-10 22:59:06 -0400

    working on gameOver

--- src/App.js
+++ src/App.js
@@ -4,23 +4,23 @@ import './App.css';
 import MemoryCard from './components/MemoryCard';
 
 function generateDeck() {
-  var symbol = ['∆', 'µ', 'å', '∫', 'ç', '∂', 'œ', '∑']; //1.2.a Set symbols equal to an array containing these characters: ∆, ß, £, §, •, $, +, ø
-  var deck = [];
-  for (let index = 0; index < 15; index++) {
-    //todo 
-    let element = deck[index];
+  const symbol = ['∆', 'µ', 'å', '∫', 'ç', '∂', 'œ', '∑']; //1.2.a Set symbols equal to an array containing these characters: ∆, ß, £, §, •, $, +, ø
+  const deck = [];
+  for (let index = 0; index < 16; index++) {
     let card = {
       isFlipped: false,
-      symbol: symbol[index%8],
+      symbol: symbol[index % 8]
     }
+    deck.push(card);
   }
   return shuffle(deck);
 };
+
 //1.8 copy and paste the shuffle method from stackover flow
 function shuffle(deck) {
   for (let i = deck.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [deck[i], deck[j]] = [deck[j], deck[i]];
+    const j = Math.floor(Math.random() * (i + 1));
+    [deck[i], deck[j]] = [deck[j], deck[i]];
   }
   return deck;
 }
@@ -41,48 +41,107 @@ function shuffle(deck) {
 // `
 
 
-
+//step 1.1.a.i
 class App extends Component {
   constructor(props) {
-    super(props);
+    super(props); //step 1.1.c
     this.state = {
       //step 1.2 Initialize the app state with 2 properties: deck and pickedCards
       //and set them as empty array
-      deck: {}, //step 1.10 make the deck property point to a call to generateDeck()
-      pickedCards: {},
+      deck: generateDeck(), //step 1.10 make the deck property point to a call to generateDeck()
+      pickedCards: []
+    }
+  }
+
+  pickCard = (cardIndex) => {
+    if (this.state.deck[cardIndex].isFlipped) {
+      return;
+    }
+    const cardToFlip = { ...this.state.deck[cardIndex] };
+    cardToFlip.isFlipped = true;
+
+    let newPickedCards = this.state.pickedCards.concat(cardIndex);
+    const newDeck = this.state.deck.map((card, index) => {
+      if (cardIndex === index) {
+        return cardToFlip
+      };
+      return card
+    });
+
+    //newPickedCards is a COPY of the card, it does not have a "state" yet -- line 84
+    if (newPickedCards.length === 2) {
+      const card1Index = newPickedCards[0]; 
+      const card2Index = newPickedCards[1];
+      if (newDeck[card1Index].symbol !== newDeck[card2Index].symbol) {
+        setTimeout(() => {this.unflipCards(card1Index, card2Index)}, 1)
+        // this.unflipCards(card1Index, card2Index); //step 7.14
+      }
+      newPickedCards = [];
     }
 
+    this.setState({ //we set the state of pickedCards here
+      deck: newDeck,
+      pickedCards: newPickedCards
+    });
   }
+
+  //7.7 member function called unflipcards
+  unflipCards = (card1Index, card2Index) => {
+    const card1 = { ...this.state.deck[card1Index] } //this weird syntax is used to clone objects. 
+    const card2 = { ...this.state.deck[card2Index] }  //clone 2x
+    card1.isFlipped = false;
+    card2.isFlipped = false;
+
+    //incoming!!!! 
+    const newDeck = this.state.deck.map((card, index) => {
+      if (card1Index === index) {
+        return card1;
+      };
+      if (card2Index === index) {
+        return card2;
+      };
+      return card;
+    });
+
+    this.setState({
+      deck: newDeck
+    });
+  }
+
+  // gameOver = () => {
+  //   if (this.state.deck.map((card) => card.isFlipped )) {
+  //     return alert('you win');
+  //   }
+  // }
+
+  //step 1.1.a.ii
   render() {
+    const cardsJSX = this.state.deck.map((card, index) => {
+      return <MemoryCard
+        symbol={card.symbol}
+        isFlipped={card.isFlipped}
+        key={index}
+        pickCard={this.pickCard.bind(this, index)}
+      />
+    });
+
     return (
       <div className="App">
         <header className="App-header">
           <h1>Memory game</h1>
-          <h4 class="app-subtitle">Match Cards to win!</h4>
+          <h4 className="app-subtitle">Match Cards to win!</h4>
         </header>
         <div>
-          <MemoryCard></MemoryCard>
-          <MemoryCard></MemoryCard>
-          <MemoryCard></MemoryCard>
-          <MemoryCard></MemoryCard>
+          {cardsJSX.slice(0, 4)}
         </div>
         <div>
-          <MemoryCard></MemoryCard>
-          <MemoryCard></MemoryCard>
-          <MemoryCard></MemoryCard>
-          <MemoryCard></MemoryCard>
+          {cardsJSX.slice(4, 8)}
         </div>
         <div>
-          <MemoryCard></MemoryCard>
-          <MemoryCard></MemoryCard>
-          <MemoryCard></MemoryCard>
-          <MemoryCard></MemoryCard>
+          {cardsJSX.slice(8, 12)}
         </div>
         <div>
-          <MemoryCard></MemoryCard>
-          <MemoryCard></MemoryCard>
-          <MemoryCard></MemoryCard>
-          <MemoryCard></MemoryCard>
+          {cardsJSX.slice(12, 16)}
         </div>
       </div>
     );
--- src/components/MemoryCard.js
+++ src/components/MemoryCard.js
@@ -4,33 +4,22 @@ import Logo from '../../src/dg.png';
 
 
 class MemoryCard extends Component { //Memory card is a component of the react app
-    constructor(props) { //save all the previous states of the objects
-        super(props);
-        this.state = { isFlipped: false }; //this is the default state for isFlipped
-    }
-
-    //whenever the user clicks this card, the state of isFlipped twists.
-    clickHandler() {
-        this.setState({
-            isFlipped: !this.state.isFlipped 
-        })
-    };
 
     //CSS takes care of this part, CSS class name = A, or A+B, 
     // innerClass is the variable to save the CSS class name. 
     render() {
         let innerClass = "MemoryCard_inner";
-        if (this.state.isFlipped) {
+        if (this.props.isFlipped) { //* very important, props is the "state" of the object past down from the parent component */
             innerClass += ' flipped'; //how do we determine flipped? 
             //it's the when the user clicks on the card (the state isflipped becomes true), so inner class = A+B.
         }
 
         return (
             //when the user clicks a card, it calls the function clickHandler which toggles the state of isFlipped
-            <div className="MemoryCard" onClick={ (e) => this.clickHandler(e) }> 
+            <div className="MemoryCard" onClick={this.props.pickCard}>
                 <div className={innerClass}>
                     <div className="MemoryCard_back"><img src={Logo} className="Logo" alt="bg" /></div>
-                    <div className="MemoryCard_front">∆</div>
+                    <div className="MemoryCard_front">{this.props.symbol}</div>
                 </div>
             </div>
 
